eb97c73b941fa39c70a4e29dda981009665067fa
Dimitri Belopopsky <dimitri@belopopsky.com>
2022-04-03 10:04:50 +0200

    Improve hiccup shake

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -3,6 +3,12 @@ extends KinematicBody2D
 var velocity := Vector2.ZERO
 const SPEED := 100.0
 
+onready var camera := $Camera2D
+
+
+var shake_amount = 1000
+
+var hiccup_shake = false
 
 func _input(event: InputEvent) -> void:
 	pass
@@ -14,12 +20,18 @@ func _physics_process(delta: float) -> void:
 
 	rotation = dir.angle()
 	
-	$Camera2D.rotation = lerp($Camera2D.rotation, 0, 0.001)
-	
+	if hiccup_shake:
+		camera.set_offset(lerp(camera.get_offset(), Vector2.ZERO, 0.1))
+
 
 
 func _on_TimerHiccup_timeout() -> void:
 	print("Hiccup!")
 	$RandomHiccupsPlayer.play()
-	$Camera2D.rotation = -2
-	
+	hiccup_shake = true
+	$HiccupShakeTimer.start()
+	camera.set_offset(Vector2(rand_range(-1.0, 1.0), -20.0))
+
+func _on_HiccupShakeTimer_timeout() -> void:
+	hiccup_shake = false
+	camera.set_offset(Vector2.ZERO)
